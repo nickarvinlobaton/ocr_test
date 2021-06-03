2eff3e94689994c2cbc1410d1f8e72b1fdf63028
Nick Arvin Lobaton <nick.l@agentsofvalue.com>
2021-06-03 15:37:59 +0800

    get response from ocr

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 (async () => {
   const vision = require("@google-cloud/vision");
 
-  const client = new vision.ImageAnnotatorClient({ keyFilename: "key.json" });
+  const config = {
+    keyFilename: "key.json",
+  };
+
+  const client = new vision.ImageAnnotatorClient(config);
 
   client
-    .textDetection("sample/original.jpg")
+    .textDetection("https://storage.googleapis.com/certovax-images/vaccination/51_vaccine_25746446a43ab00235ed79c1a7d87175.jpg")
     .then((results) => {
       const result = results[0].textAnnotations;
 
